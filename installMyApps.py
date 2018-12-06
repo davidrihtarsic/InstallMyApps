2a0aaba9f9a5b4f6f7d12917a83a61f723a7004c
tomaz <tomaz@debian>
2018-12-06 22:06:26 +0100

    LibreOffice 6.1.3 posodobljen

--- installMyApps.py
+++ installMyApps.py
@@ -896,12 +896,12 @@ def Install_programms():
 	LibreOffice.description = 'Office suit for linux and other OS...'
 	LibreOffice.apt_get_name =''
 	LibreOffice.check_version_cmd = ''
-	LibreOffice.tar_package_path_32 = 'https://download.documentfoundation.org/libreoffice/stable/6.1.2/deb/x86/'
-	LibreOffice.tar_package_file_32 = 'LibreOffice_6.1.2_Linux_x86_deb.tar.gz'
+	LibreOffice.tar_package_path_32 = 'https://download.documentfoundation.org/libreoffice/stable/6.1.3/deb/x86/'
+	LibreOffice.tar_package_file_32 = 'LibreOffice_6.1.3_Linux_x86_deb.tar.gz'
 	LibreOffice.deb_package_path_64 = ''
 	LibreOffice.deb_package_file_64 = ''
-	LibreOffice.tar_package_path_64 = 'https://download.documentfoundation.org/libreoffice/stable/6.1.2/rpm/x86_64'
-	LibreOffice.tar_package_file_64 = 'LibreOffice_6.1.2_Linux_x86-64_deb.tar.gz'
+	LibreOffice.tar_package_path_64 = 'https://download.documentfoundation.org/libreoffice/stable/6.1.3/rpm/x86_64'
+	LibreOffice.tar_package_file_64 = 'LibreOffice_6.1.3_Linux_x86-64_deb.tar.gz'
 	LibreOffice.tar_destination =''
 	LibreOffice.tar_extra_cmds = ['sudo dpkg -i '+ download_dir +'LibreOffice_*_Linux_*_deb/DEBS/*.deb']
 	VsiProgrami.append(LibreOffice.program_name)
